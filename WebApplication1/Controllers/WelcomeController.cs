df565cf215690af96e97a30d42193b7d6a632c3a
Jordan M <jmcclusky17@gmail.com>
2018-04-21 13:58:28 -0500

    Added WelcomeController and Welcome.index.cshtml

---
+++ WebApplication1/Controllers/WelcomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Index()
+        {
+            string c00kie = "true";
+            if (c00kie == "true")
+                {
+                // TODO - Have ViewModel insert "User Home" and "LogOut" links if cookie for user session is detected.
+                return View();
+            }
+            else
+            {
+                // TODO - if no cookie for user session, insert "Member" and "Become A Member" links.
+                return View();
+            }
+        }
+
+        // GET: Welcome/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Welcome/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Welcome/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Welcome/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Welcome/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Welcome/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Welcome/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
