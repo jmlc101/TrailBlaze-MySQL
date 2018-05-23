78af2f85b607d9ece2d225fc1220a310957c8129
Jordan M <jmcclusky17@gmail.com>
2018-05-22 19:35:03 -0500

    ConfirmSubmission and Serialization: RouteControlller, added SubmitRoute, created SubmitRoute.cshtml made Route Model Serializable serialized newRoute and deserialized in Route Controller Redirected SaveRoute to /SubmitRoute created SubmitRouteViewModel.

---
+++ WebApplication1/ViewModels/SubmitRouteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ViewModels
+{
+    public class SubmitRouteViewModel
+    {
+        public int UserId { get; set; }
+    }
+}
